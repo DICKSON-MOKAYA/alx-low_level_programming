ba4e6f3c16545a150a14d5f1898dc538d3e0ca8a
DICKSON-MOKAYA <dicksononsomu@gmail.com>
2023-06-15 11:07:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,9 @@
  * Return: Always 1 (success)
  */
 int main(void)
-{
+{	
+	char Dora_Korpar
+
         printf("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n");
         return (1);
 } 
