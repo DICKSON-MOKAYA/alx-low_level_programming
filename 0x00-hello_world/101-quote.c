bbc392c7b4ce0f3a93803397255d69c03def53cd
DICKSON-MOKAYA <dicksononsomu@gmail.com>
2023-06-15 11:11:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Return: Always 1 (success)
+ *main - entry point
+ *Return: Always 1 (success)
  */
 int main(void)
 {	
 	char Dora_Korpar
-
         printf("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n");
         return (1);
 } 
