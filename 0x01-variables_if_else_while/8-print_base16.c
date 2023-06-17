558577207287641918f753161fefc19d4ea1c67a
DICKSON-MOKAYA <dicksononsomu@gmail.com>
2023-06-17 06:55:50 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-        int num;
+	int num;
 	char letter;
 
-        for (num = 0; num < 10; num++)
-                putchar((num % 10) + '0');
+	for (num = 0; num <= 10; num++)
+		putchar((num % 10) + '0');
 	for (letter = 'a', letter <= 'f', letter++)
 	
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
