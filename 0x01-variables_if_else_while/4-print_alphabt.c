b7bc555f98c414597410c99edeec182144bf1c7a
DICKSON-MOKAYA <dicksononsomu@gmail.com>
2023-06-17 04:17:24 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+	if (lowercase == 'e' || lowercase == 'q')
+	{
+	lowercase += 1;
+	}
+	else
+	{
+	putchar(uppercase);
+	uppercase += 1;
+	}
+	putchar('\n');
+	return (0);
+}
