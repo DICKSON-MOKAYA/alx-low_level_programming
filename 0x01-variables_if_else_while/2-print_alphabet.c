247cb003d3d5f26ccc72ddceb25fcc4e4dd789d7
DICKSON-MOKAYA <dicksononsomu@gmail.com>
2023-06-17 02:48:53 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+        {
+        int lowercase = 'a';
+	while (lowercase <= 'z');
+	
+        {
+                putchar (lowercase);
+		lowecase += 1;
+        }
+        putchar ('\n');
+        return (0);
+	}
