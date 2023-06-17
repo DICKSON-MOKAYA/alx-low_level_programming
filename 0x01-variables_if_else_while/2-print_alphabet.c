c88d20f1e38d9594091f5efc2b97621bdd4ced56
DICKSON-MOKAYA <dicksononsomu@gmail.com>
2023-06-17 03:09:26 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-    int lowercase = 'a';
-    while (lowercase <= 'z')
-    {
-        putchar(lowercase);
-        lowercase += 1;
-    }
-    putchar('\n');
-    return (0);
+	int lowercase = 'a';
+	while (lowercase <= 'z')
+	{
+	putchar(lowercase);
+	lowercase += 1;
+	}
+	putchar('\n');
+	return (0);
 }
