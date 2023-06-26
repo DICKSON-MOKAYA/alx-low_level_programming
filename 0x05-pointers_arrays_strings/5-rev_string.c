3194986874a5f6899d59c862c170093922973920
DICKSON-MOKAYA <dicksononsomu@gmail.com>
2023-06-26 04:50:57 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
-* rev_string - Reverses a string
-*@s: The string to be modified
-*Return: void
-*/
+ *rev_string - Reverses a string
+ *@s: The string to be modified
+ *Return: void
+ */
 void rev_string(char *s)
+
 {
 
 	int len = 0, index = 0;
 	char tmp;
 	
-	while (s[index++])
-	{
+	while (s[index++])	
 	len++;
 	
 	for (index = len -1; index >= len / 2; index--)
@@ -19,6 +19,5 @@ void rev_string(char *s)
 	tmp = s[index];
 	s[index] = s[len - index - 1];
 	s[len - index - 1] = tmp;
-	}
 	}
 }
