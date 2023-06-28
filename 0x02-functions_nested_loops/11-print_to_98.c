558df19d4c593d30f8dcda1f8b27680a03aae926
DICKSON-MOKAYA <dicksononsomu@gmail.com>
2023-06-28 05:05:56 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,15 @@
 *Return: Always 0.
 */
 
-void print_to_98 (int n)
+void print_to_98(int n)
 {
 	if (n <= 98)
 	{
 	for (; n <= 98; n++)
 	{
 	if (n == 98)
-	{	
-	printf("%d", n) ;
+	{
+	printf("%d", n);
 	printf("\n");
 	break;
 	}
@@ -31,7 +31,7 @@ void print_to_98 (int n)
 	{
 	if (n == 98)
 	{
-	printf("%d", n) ;
+	printf("%d", n);
 	printf("\n");
 	break;
 	}
