11566e66644f3c1b294e494084284cf3c822df3e
DICKSON-MOKAYA <dicksononsomu@gmail.com>
2023-06-28 05:35:07 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 */
 void times_table(void)
 {
-	int num, mult, prod; 
+	int num, mult, prod;
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
@@ -14,7 +14,7 @@ void times_table(void)
 		_putchar(' ');
 
 		prod = num * mult;
-		
+
 		if (prod <= 9)
 			_putchar(' ');
 		else
